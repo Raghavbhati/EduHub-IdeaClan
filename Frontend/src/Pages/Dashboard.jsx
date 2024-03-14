3c3483862febbc6fce5cf3dfd6f6b9fb17141c3c
Raghavbhati <bhatiraghav4@gmail.com>
2024-03-15 01:10:38 +0530

    Readme Images Added

--- Frontend/src/Pages/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard.jsx
@@ -18,14 +18,15 @@ const Dashboard = () => {
   const [courses, setCourses] = useState([]);
   const { loading, error, data } = useQuery(GET_COURSES);
   const auth = useContext(AuthContext);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :</p>;
 
   useEffect(() => {
+    if (loading) return; 
+    if (error) return;
     if (data && data.courses) {
       setCourses(data.courses);
     }
-  }, [data]);
+  }, [loading, error, data]);
+
   return (
     <>
       <div className="sticky top-0 left-0">
@@ -33,23 +34,34 @@ const Dashboard = () => {
       </div>
       <div className="w-full py-10 bg-gray-100">
         <div className="w-10/12 m-auto rounded-lg py-15 bg-white">
-          <h2 className="text-center pb-16 pt-10 text-4xl font-semibold text-dark">Welcome to Dashboard</h2>
+          <h2 className="text-center pb-16 pt-10 text-4xl font-semibold text-dark">
+            Welcome to Dashboard
+          </h2>
           <div className="px-4">
             <div className="flex gap-2 flex-col">
-              {data.courses.map(({ id, name, description, prerequisites }) => (
-                <div className="bg-gray-100 p-3 rounded-md flex justify-between align-middle items-center" key={id}>
-                  <div className="">
-                    <p className="text-xl text-text-orange font-semibold">{name}</p>
-                    <p className="text-sm font-normal">{description}</p>
+              {data &&
+                data.courses &&
+                data.courses.map(({ id, name, description, prerequisites }) => (
+                  <div
+                    className="bg-gray-100 p-3 rounded-md flex justify-between align-middle items-center"
+                    key={id}
+                  >
+                    <div className="">
+                      <p className="text-xl text-text-orange font-semibold">
+                        {name}
+                      </p>
+                      <p className="text-sm font-normal">{description}</p>
+                    </div>
+                    <div>
+                      <p>{`Prerequisites: ${prerequisites}`}</p>
+                    </div>
+                    <div>
+                      <button className="py-2 px-5 bg-dark rounded-md text-light font-medium hover:text-white hover:bg-primary ">
+                        Enroll now
+                      </button>
+                    </div>
                   </div>
-                  <div>
-                    <p>{`Prerequisites: ${prerequisites}`}</p>
-                  </div>
-                  <div>
-                    <button className="py-2 px-5 bg-dark rounded-md text-light font-medium hover:text-white hover:bg-primary ">Enroll now</button>
-                  </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         </div>
